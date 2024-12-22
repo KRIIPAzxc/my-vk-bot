c5a9cc09ed10dc49db8a0f13f70eee3a0714005c
KRIIPA <140049050+KRIIPAzxc@users.noreply.github.com>
2024-12-23 01:00:52 +0800

    Add files via upload

---
+++ VK/commands/help.py
@@ -0,0 +1,44 @@
+import json
+import vk_api
+from vk_api.utils import get_random_id
+
+
+def main(event,vk):
+    try:
+        with open('./data.json', 'r', encoding='utf-8') as json_file:
+            datatemp = json.load(json_file)
+        language = datatemp[str(event.user_id)]["lang"]
+    except:
+        language = "en"
+        
+    try:
+
+        text = ""
+        eventText = str(event.text).split()
+        print(eventText)
+        with open('./commands/command.json', 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+
+        for item in data[language]:
+            try:
+                if eventText[1] in item['read']:
+                    text = "/" + item["read"] + " - " + item["alldescription"] + "\n"
+                    vk.messages.send(
+                        peer_id=event.peer_id,
+                        random_id=get_random_id(),
+                        message=text
+                                    )
+                    return "done"
+                else:
+                    0/0
+            except:
+                text += "/" + item["read"] + " - " + item["description"] + "\n"
+
+        vk.messages.send(
+            peer_id=event.peer_id,
+            random_id=get_random_id(),
+            message=text
+        )
+        return "done"
+    except Exception as e:
+        return "script error :" + str(e)
---
+++ VK/commands/translator.py
@@ -0,0 +1,106 @@
+import json
+import time
+import requests
+import vk_api
+from vk_api.utils import get_random_id
+
+import langs
+
+def main(event,vk):
+    try:
+        with open('./data.json', 'r', encoding='utf-8') as json_file:
+            datatemp = json.load(json_file)
+        language = datatemp[str(event.user_id)]["lang"]
+    except:
+        language = "en"
+
+    try:
+        eventText = str(event.text).split()
+        inputText = ""
+        lang = langs.languages()
+        
+        with open('./commands/command.json', 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+            data = data["output"]
+            
+        if eventText[1] in lang:
+            print()
+            inputText = ' '.join(eventText[2:])
+            outputText = translate(inputText,eventText[1])
+        
+            vk.messages.send(
+            peer_id=event.peer_id,
+            random_id=get_random_id(),
+            message=outputText.lower()
+            )
+        else:
+            vk.messages.send(
+            peer_id=event.peer_id,
+            random_id=get_random_id(),
+            message=data[language]["lanNotSup"]
+            )
+
+        return "done"
+    except Exception as e:
+        return "script error :" + str(e)
+    
+def auto(event,vk,):
+    try:
+        with open('./data.json', 'r', encoding='utf-8') as json_file:
+            datatemp = json.load(json_file)
+        language = datatemp[str(event.user_id)]["lang"]
+        lang = datatemp[str(event.user_id)]["autoLang"]
+    except:
+        language = "en"
+
+    try:
+        inputText = ""
+        with open('./commands/command.json', 'r', encoding='utf-8') as json_file:
+            data = json.load(json_file)
+            data = data["output"]
+
+        inputText = str(event.text)
+        print()
+        outputText = translate(inputText,lang)
+        
+        vk.messages.send(
+        peer_id=event.peer_id,
+        random_id=get_random_id(),
+        message=outputText.lower()
+        )
+
+        return "done"
+    except Exception as e:
+        vk.messages.send(
+        peer_id=event.peer_id,
+        random_id=get_random_id(),
+        message=data[""]
+        )
+        return "script error :" + str(e)
+    
+def translate(text, target_language):
+    
+    url = "https://translate.googleapis.com/translate_a/single"
+
+    params = {
+        "client": "gtx",
+        "sl": "auto",
+        "tl": target_language,
+        "dt": "t",
+        "q": text
+    }
+
+    response = requests.get(url, params=params)
+    try:
+        if response.status_code == 200:
+            result = response.json()
+            translated_text = result[0][0][0]
+            return translated_text
+        else:
+            print("wait")
+            time.sleep(5)
+            translate(text, target_language)
+    except:
+        print("wait")
+        time.sleep(5)
+        translate(text, target_language)
